266a35afeb8778f5e56aa22c8d7945c822671a44
Sofus Lund <sofuslund.dev@gmail.com>
2024-06-13 15:18:59 +0200

    Update

--- src/app/components/JobCard.js
+++ src/app/components/JobCard.js
@@ -3,19 +3,19 @@ import Image from "next/image";
 export default function JobCard({data, addTag}) {
     const tags = [data.role, data.level, ...data.languages, ...data.tools].map(tag => {
         return (
-            <span onClick={()=>{addTag(tag)}} className="rounded-md bg-light-gray text-[1.05rem] font-bold align-sub text-cyan px-3 py-0.5 inline-block cursor-pointer">{tag}</span>
+            <span onClick={()=>{addTag(tag)}} className="rounded-md bg-light-gray hover:bg-cyan text-[1.05rem] font-bold align-sub text-cyan hover:text-white px-2 py-0.5 inline-block cursor-pointer">{tag}</span>
         );
     });
 
     const border = data.featured ? "border-cyan box-border border-l-[.35rem]" : "";
     
     const newTag = data.new ? <span className="bg-cyan mr-2 ml-6 text-white rounded-2xl px-2 pt-2 pb-1">NEW!</span> : null;
-    const featuredTag = data.featured ? <span className="bg-dark-gray text-white rounded-2xl px-2 pt-2 pb-1">FEATURED!</span> : null;
+    const featuredTag = data.featured ? <span className="bg-dark-gray text-white rounded-2xl px-2 pt-2 pb-1">FEATURED</span> : null;
 
     return (
-        <div className={`rounded-md bg-white w-[88%] mx-auto p-6 md:px-12 my-10 shadow-md md:flex gap-5 items-center justify-between ${border}`}>
+        <div className={`rounded-md bg-white w-[88%] max-w-6xl mx-auto py-6 px-5 md:px-12 my-10 shadow-md md:flex gap-5 items-center justify-between ${border}`}>
             <div className="md:flex items-center flex-shrink-0 gap-6">
-                <Image className="rounded-full -mt-11 md:mt-0 md:w-20 md:h-20" src={data.logo} width="45" height="45">
+                <Image className="rounded-full -mt-11 md:mt-0 md:w-20 md:h-20" src={data.logo} width="48" height="48">
                 </Image>
                 <div>
                     
@@ -24,7 +24,7 @@ export default function JobCard({data, addTag}) {
                         {newTag}
                         {featuredTag}
                     </h2>
-                    <h1 className="font-bold my-2" >{data.position}</h1>
+                    <h1 className="font-bold my-2 text-lg hover:text-cyan cursor-pointer" >{data.position}</h1>
                     <span className="font-semibold text-gray my-2">{data.postedAt} &#x2022; {data.contract} &#x2022; {data.location}</span>
                     <hr className="border-gray my-4 md:hidden"></hr>
                 </div>
--- src/app/components/TagContainer.js
+++ src/app/components/TagContainer.js
@@ -7,7 +7,7 @@ export default function TagContainer({tags, setTags}) {
         return (
             <div className="rounded-md bg-light-gray overflow-hidden">
                 <span className="text-[1.05rem] font-bold align-sub text-cyan px-2 inline-block">{tag}</span>
-                <div className="bg-cyan w-8 h-8 align-middle inline-flex items-center justify-center cursor-pointer" onClick={()=>setTags(new Set([...tags].filter(t=>t!=tag)))}>
+                <div className="bg-cyan hover:bg-dark-gray w-8 h-8 align-middle inline-flex items-center justify-center cursor-pointer" onClick={()=>setTags(new Set([...tags].filter(t=>t!=tag)))}>
                     <Image
                         className="inline-block"
                         src="/icon-remove.svg"
@@ -22,9 +22,9 @@ export default function TagContainer({tags, setTags}) {
     const hidden = tagElms.length > 0 ? "" : "hidden";
 
     return (
-        <div className={`bg-white relative w-[88%] mx-auto rounded-md py-6 px-5 -my-10 mb-10 flex justify-between items-center shadow-md ${hidden}`}> 
+        <div className={`bg-white relative w-[88%] max-w-6xl mx-auto rounded-md py-6 px-5 -my-10 mb-10 flex justify-between items-center shadow-md ${hidden}`}> 
             <div className="flex flex-wrap gap-4">{tagElms}</div>
-            <span onClick={()=>setTags(new Set([]))} className="text-[1.03rem] mx-1 font-bold text-gray align-bottom h-[1.5rem] cursor-pointer">Clear</span>
+            <span onClick={()=>setTags(new Set([]))} className="text-[1.03rem] mx-1 font-bold text-gray hover:text-cyan hover:underline align-bottom h-[1.5rem] cursor-pointer">Clear</span>
         </div>
     );
 }
